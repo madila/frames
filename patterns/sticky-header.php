4278a9b4fca98db1742555216ea48d3627652677
Ruben <contact@rubenmadila.com>
2023-08-21 09:38:38 +0100

    Add more styles
    Remove unnecessary font files. Replace some patterns and template-parts.

---
+++ patterns/sticky-header.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Sticky Header
+ * Slug: frames/sticky-header
+ * Categories: header
+ * Keywords: sticky
+ * Block Types: core/template-part/header
+ */
+?>
+<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"},"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
+<div id="wp-site-header" class="wp-block-group has-white-background-color has-background" style="padding-right:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:template-part {"slug":"header","theme":"frames","tagName":"header"} /--></div>
+<!-- /wp:group -->
