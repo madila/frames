607fed710c30c61269927fe9ce5bd19b9b812453
Ruben <contact@rubenmadila.com>
2024-03-21 12:33:13 +0000

    Uupdate

--- src/block-library/index.js
+++
@@ -1,7 +0,0 @@
-import './add-animation-controls/addAnimationAttribute';
-import './add-animation-controls/addAnimationSetting';
-import './add-animation-controls/addAnimationProp';
-import './add-animation-controls/addAnimationSave';
-
-
-//import './card/register';
